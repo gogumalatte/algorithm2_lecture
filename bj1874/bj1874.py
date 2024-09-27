20b343cc9d122a1a0f31aeff7683b2554f31547e
kiyeong <kjr07145@gmail.com>
2024-09-27 17:27:32 +0900

    9월 27일 금요일 백준 1sol

---
+++ bj1874/bj1874.py
@@ -0,0 +1,33 @@
+import sys
+
+def 백준1874():
+    n = int(input())  # 입력 받을 숫자의 개수
+    sequence = [int(sys.stdin.readline()) for _ in range(n)]  # 입력 수열
+    
+    stack = []  # 스택
+    answer = []  # 출력할 결과 리스트
+    current = 1  # 스택에 넣을 숫자
+    possible = True  # 수열을 만들 수 있는지 여부
+    
+    for num in sequence:
+        # 입력된 숫자보다 작거나 같은 숫자를 스택에 넣음
+        while current <= num:
+            stack.append(current)
+            answer.append('+')
+            current += 1
+        
+        # 스택의 마지막 숫자가 입력 숫자와 같으면 pop
+        if stack and stack[-1] == num:
+            stack.pop()
+            answer.append('-')
+        else:
+            possible = False
+            break
+    
+    if possible:
+        # 각 결과를 출력
+        print('\n'.join(answer))
+    else:
+        print("NO")
+
+백준1874()
